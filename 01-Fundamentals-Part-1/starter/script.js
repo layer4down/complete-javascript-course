68dfb7fa4ee47678d6170edb8e53a9f2c9eee784
layer4down <layer4down@users.noreply.github.com>
2024-02-11 16:56:35 -0500

    modified first lab

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,4 @@
+let largeNumber = Number.MAX_SAFE_INTEGER; // 9007199254740991
+console.log(largeNumber + 1); // Output: 9007199254740992 
+console.log(largeNumber + 2); // Output: 9007199254740992 
+console.log(largeNumber + 3); // Output: 9007199254740994
